ebe1bafaa4b39085841f7c78b1471bb44aa3b0ab
LuiliXain <76813280+LuiliXain@users.noreply.github.com>
2022-05-27 15:47:13 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,18 @@
 import { memo } from 'react';
 
 import style from './index.module.scss';
-
+import SlideShow from './slide/index.js';
 
 const HomePage = memo(() => {
   return (
-    <div
-      className={style.div}
-    >
-      Home
-    </div>
+    <>
+      <div className={style.titlebar}>
+        <img src="" alt="" />
+      </div>
+      <div className={style.slideShow}>
+        <SlideShow></SlideShow>
+      </div>
+    </>
   )
 })
 
