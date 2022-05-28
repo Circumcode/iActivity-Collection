9caf0bced0a96388a6d1efeaa73f75b9ef747e88
chengHan <75972336+chengHan1222@users.noreply.github.com>
2022-05-28 15:17:20 +0800

    update 2022/05/28

--- src/pages/Home/Calendar/index.tsx
+++ src/pages/Home/Calendar/index.tsx
@@ -12,10 +12,10 @@ export default class index extends Component {
                 </div>
                 <br/>
 				<div className={style.season}>
-					<a href="#sesson0">1-3月</a>
-					<a href="#sesson1">4-6月</a>
-					<a href="#sesson2">7-9月</a>
-					<a href="#sesson3">10-12月</a>
+					<a href="#spring">1-3月</a>
+					<a href="#summer">4-6月</a>
+					<a href="#fall">7-9月</a>
+					<a href="#winter">10-12月</a>
 				</div>
 			</div>
 		);
