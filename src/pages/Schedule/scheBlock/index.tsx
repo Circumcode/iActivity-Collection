2ec1b9c446db1d4b93fcd635b8ccb0fca50cc78a
LuiliXain <76813280+LuiliXain@users.noreply.github.com>
2022-05-29 15:22:04 +0800

    update 2022/05/29

--- src/pages/Schedule/scheBlock/index.tsx
+++ src/pages/Schedule/scheBlock/index.tsx
@@ -9,21 +9,17 @@ class scheDetail {
     uid: string;
     title: string;
     space: string;
-    time: string;
-    endTime: string;
     weather: string;
-    eventStartTime: string;
-    eventEndTime: string;
+    startTime: string;
+    endTime: string;
 
     constructor(uid: string, title: string, space: string, time: string, endTime: string, weather: string){
         this.uid = uid;
         this.title = title;
         this.space = space;
-        this.eventStartTime = time;
-        this.eventEndTime = endTime;
+        this.startTime = time;
+        this.endTime = endTime;
         this.weather = weather;
-        this.time = "0/0/0 0:0:0";
-        this.endTime = "0/0/0 0:0:0";
     }
 }
 
@@ -42,54 +38,86 @@ export default class ScheBlock extends Component<IProps, IState> {
         }
     }
 
+    componentDidMount(){
+        this.setScheDetails();
+    }
+
     cancel = (id: string) => {
         Activity.cancel(id);
+        this.setScheDetails();
     }
 
     setScheDetails(){
+        let newScheDetails:any = [];
         let response = Activity.getReserved();
-        for (let i=0; i<response.length; i++){
-            let weather = "";
-            let flag = true;
-            let startTime = response[i].activity.showInfo[0].time;
-            let endTime = response[i].activity.showInfo[0].endTime;
-            WeatherAPIUtils.getByLocation(response[i].activity.showInfo[0].city, response[i].activity.showInfo[0].area)
-            .then((data) =>{
-                if (data !== undefined) {
-                    for (let j=0; j<data.length; j++){
-                        let weatherStart = data[i].startTime.split(" ")[0].split("-");
-                        let weatherEnd = data[i].endTime.split(" ")[0].split("-");
-                        let eventTime = startTime.split(" ")[0].split("/");
-                        for (let index=0; index<3; index++){
-                            if (Number(weatherStart[index]) <= Number(eventTime[index]) && 
-                            Number(weatherEnd[index]) >= Number(eventTime[index])) continue;
-                            else {
-                                flag = false;
-                                break;
+        if (response.length != 0){
+            for (let i=0; i<response.length; i++){
+                let weather = "";
+                let flag = true;
+                let startTime = response[i].activity.showInfo[0].time;
+                let endTime = response[i].activity.showInfo[0].endTime;
+                WeatherAPIUtils.getByLocation(response[i].activity.showInfo[0].city, response[i].activity.showInfo[0].area)
+                .then((data) =>{
+                    if (data !== undefined) {
+                        for (let j=0; j<data.length; j++){
+                            let weatherStart = data[i].startTime.split(" ")[0].split("-");
+                            let weatherEnd = data[i].endTime.split(" ")[0].split("-");
+                            let eventTime = startTime.split(" ")[0].split("/");
+                            for (let index=0; index<3; index++){
+                                if (Number(weatherStart[index]) <= Number(eventTime[index]) && 
+                                Number(weatherEnd[index]) >= Number(eventTime[index])) continue;
+                                else {
+                                    flag = false;
+                                    break;
+                                }
                             }
+                            if (flag) weather = data[i].values.Wx;
                         }
-                        if (flag) weather = data[i].values.Wx;
                     }
-                }
-                if (flag === false) weather = "尚未有天氣預報";
-                
-                this.state.scheDetails[i] = new scheDetail(
-                    response[i].activity.UID,
-                    response[i].activity.title,
-                    response[i].activity.showInfo[0].location,
-                    startTime, 
-                    endTime,
-                    weather);
+                    if (flag === false) weather = "尚未有天氣預報";
+
+                    console.log(response[i].dateStart);
+
+                    newScheDetails.push(new scheDetail(
+                        response[i].activity.UID,
+                        response[i].activity.title,
+                        response[i].activity.showInfo[0].location,
+                        (!response[i].dateStart) ? "0000-00-00T00:00:00": String(response[i].dateStart),
+                        (!response[i].dateEnd) ? "0000-00-00T00:00:00": String(response[i].dateEnd),
+                        weather))
+
+                    if (newScheDetails != null){
+                        this.setState({
+                            scheDetails: newScheDetails,
+                        })
+                    }
+                }).catch((error)=>{
+                    newScheDetails.push(new scheDetail(
+                        response[i].activity.UID,
+                        response[i].activity.title,
+                        response[i].activity.showInfo[0].location,
+                        (!response[i].dateStart) ? "0000-00-00T00:00:00": String(response[i].dateStart),
+                        (!response[i].dateEnd) ? "0000-00-00T00:00:00": String(response[i].dateEnd),
+                        "尚未有天氣預報"))
+
+                    if (newScheDetails != null){
+                        this.setState({
+                            scheDetails: newScheDetails,
+                        })
+                    }
+                })
+            }
+        } else {
+            this.setState({
+                scheDetails: newScheDetails,
             })
         }
     }
 
     getSche(){
-        this.setScheDetails();
         let sches = [];
         for (let i=0; i<this.state.scheDetails.length; i++){
-            sches[i] = <Sche {...this.state.scheDetails[i]} cancel={this.cancel} key={i}/>
-            console.log(this.state.scheDetails[i]);
+            sches[i] = <Sche {...this.state.scheDetails[i]} cancel={this.cancel} update={this.setScheDetails} key={i}/>
         }
         return sches;
     }
